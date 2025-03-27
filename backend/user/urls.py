8907cb583e595324ff130af533fa22736e64232d
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-03-27 23:38:12 +0200

    Added logout logic to the backend working with the refresh token

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import RegisterView
+from user.views import RegisterView, LogoutView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
