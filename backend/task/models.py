f0e46a9901a8bd02934d6aa4edb011aca8a7faa7
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-03-29 15:12:37 +0200

    Added Task model

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from project.models import Project
+from user.models import CustomUser as User
+
+
+class Task(models.Model):
+    TASK_STATUS = [
+        ('planning', 'Planning'),
+        ('to_do', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=TASK_STATUS, default='planning')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                    related_name='assigned_tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
