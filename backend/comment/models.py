893802800d0dc7080e1940c5b506ab64aa12242a
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-03-29 15:15:50 +0200

    Added Comment model

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from user.models import CustomUser as User
+
+from task.models import Task
+
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    comment = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email}: {self.content[:30]}"
