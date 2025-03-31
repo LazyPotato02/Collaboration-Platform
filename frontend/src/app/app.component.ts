6359bcf26970aadd7ec1f147757d844df96778c2
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2025-04-01 00:13:10 +0300

    Added fetch for projects and visualized them on the sidebar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink, RouterOutlet} from '@angular/router';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {AuthService} from './auth/services/auth.services';
+import {ProjectServices} from './services/projects/project.services.service';
 
 @Component({
   selector: 'app-root',
-    imports: [RouterOutlet, RouterLink, NgIf],
+    imports: [RouterOutlet, RouterLink, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+    projects: any[] = []
+
+
     get isLoggedIn(): boolean {
         return this.authService.isAuthenticated();
     }
+    constructor(private authService: AuthService,private projectService: ProjectServices, private router: Router) {}
 
-    constructor(private authService: AuthService, private router: Router) {}
+    ngOnInit(): void {
+        this.projectService.getProjects().subscribe({
+            next: (data) => {
+                this.projects = data;
+            },
+            error: (err) => {
+                console.error('Error loading projects:', err);
+            }
+        });
+    }
 
     logout(): void {
         this.authService.logout();
